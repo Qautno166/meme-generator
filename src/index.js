c58bb23b37cbe455ecff1d41aa1742bee1629122
Qautno166 <105589257+Qautno166@users.noreply.github.com>
2023-05-13 16:43:19 +0200

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+// import "./index.css";
+// 1st Challange
+// const page = (
+//   <div>
+//     <h1> To jest moja znakomita strona</h1>
+//     <h3>Będę tutaj poznawal React</h3>
+//     <ol>
+//       <li> It's composable</li>
+//       <li>It's hirable skill</li>
+//       <li>It's only for the skillest people</li>
+//       <li>It's declatative</li>
+//     </ol>
+//   </div>
+// );
+// ReactDOM.render(page, document.getElementById("root"));
 
-const page = (
+// 2nd Challeng
+
+const first = (
   <div>
-    <h1> To jest moja znakomita strona</h1>
-    <h3>Będę tutaj poznawal React</h3>
-    <ol>
-      <li> It's composable</li>
-      <li>It's hirable skill</li>
-      <li>It's only for the skillest people</li>
-      <li>It's declatative</li>
-    </ol>
+    <img className="pic" src="logo.png" alt="" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originaly created by Jordan Walke</li>
+      <li>Has well over 100k stars on GitHub</li>
+      <li>Is maintainde by Facebook</li>
+      <li>Powers thousand of enterprise apps, including mobile apps</li>
+    </ul>
   </div>
 );
-ReactDOM.render(page, document.getElementById("root"));
+ReactDOM.render(first, document.getElementById("root"));
